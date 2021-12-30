0275b9c4917dcb763b07605b6282de0fc9d9e16b
matthew chimenti <matthewosu@gmail.com>
2021-12-30 12:34:39 -0800

    have all the non sharp notes

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-
+const noteC = document.getElementById('noteC')
+const noteD = document.getElementById('noteD')
+const noteE = document.getElementById('noteE')
+const noteF = document.getElementById('noteF')
+const noteG = document.getElementById('noteG')
 const noteA = document.getElementById('noteA')
 const noteB = document.getElementById('noteB')
-const noteG = document.getElementById('noteG')
+const noteCHigh = document.getElementById('noteCHigh')
 
+noteC.addEventListener('click', (e) => {
+    new Audio('./sounds/cLow.mp3').play()
+})
 
+noteD.addEventListener('click', (e) => {
+    new Audio('./sounds/d.mp3').play()
+})
 
+noteE.addEventListener('click', (e) => {
+    new Audio('./sounds/e.mp3').play()
+})
+
+noteF.addEventListener('click', (e) => {
+    new Audio('./sounds/f.mp3').play()
+})
+
+noteG.addEventListener('click', (e) => {
+    new Audio('./sounds/g.mp3').play()
+})
 
 noteA.addEventListener('click', (e) => {
     new Audio('./sounds/a.mp3').play()
@@ -14,9 +35,12 @@ noteB.addEventListener('click', (e) => {
     new Audio('./sounds/b.mp3').play()
 })
 
-noteG.addEventListener('click', (e) => {
-    new Audio('./sounds/g.mp3').play()
+noteCHigh.addEventListener('click', (e) => {
+    new Audio('./sounds/cHigh.mp3').play()
 })
+
+
+
 
 const playList = ['a', 'b', 'g']
 
