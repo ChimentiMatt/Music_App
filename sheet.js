3935971c955b35172464a6b7eaa61d296d3e32fc
matthew chimenti <matthewosu@gmail.com>
2021-12-30 17:58:46 -0800

    c note in first column shows other notes visuals going away with building the song

--- sheet.js
+++ sheet.js
@@ -1,6 +1,13 @@
-const c = document.getElementById('c')
-const b = document.getElementById('b')
+const c = document.getElementById('c1')
+const d = document.getElementById('d1')
+const e_ = document.getElementById('e1')
+const f = document.getElementById('f1')
+const g = document.getElementById('g1')
+const a = document.getElementById('a1')
+const b = document.getElementById('b1')
 
+const c2 = document.getElementById('c2')
+const b2 = document.getElementById('b2')
 const playList = [null]
 
 
@@ -14,16 +21,65 @@ playSequence.addEventListener('click', (e) => {
     }
 })
 
+
 c.addEventListener('click', (e) => {
     playList[0] = 'c'
-    // console.log(playList)
+    c.style.opacity = 1
+    clearNotes(1, 'c1')
+})
+
+d.addEventListener('click', (e) => {
+    playList[0] = 'd'
+    d.style.opacity = 1
+})
+
+e_.addEventListener('click', (e) => {
+    playList[0] = 'e'
+    e_.style.opacity = 1
+})
+
+f.addEventListener('click', (e) => {
+    playList[0] = 'f'
+    console.log(playList)
+    f.style.opacity = 1
+})
+
+g.addEventListener('click', (e) => {
+    playList[0] = 'c'
+    g.style.opacity = 1
+})
+
+a.addEventListener('click', (e) => {
+    playList[0] = 'a'
+    a.style.opacity = 1
 })
 
 b.addEventListener('click', (e) => {
     playList[0] = 'b'
-    console.log(playList)
-  
+    b.style.opacity = 1
+})
+
+
+c2.addEventListener('click', (e) => {
+    playList[1] = 'c'
+    c2.style.opacity = 1
+})
+
+b2.addEventListener('click', (e) => {
+    playList[1] = 'b'
+    b2.style.opacity = 1
 })
 
 
 
+const notes = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+function clearNotes(column, cn) {
+    console.log(column)
+    for (let i = 0; i < notes.length; i++){
+        let idStr = notes[i] + column.toString()
+        let x = document.getElementById(idStr)
+        x.style.opacity = .2
+        console.log(notes[1], column)
+    }
+    document.getElementById(cn).style.opacity = 1
+}
